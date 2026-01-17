73b6de63b7e8eedcce3b011ff0445b018290df39
stu <stuartorchard8@gmail.com>
2026-01-17 13:16:06 +1100

    initial commit

---
+++ Emerge/app/src/main/kotlin/App.kt
@@ -0,0 +1,11 @@
+package org.example.app
+
+import org.example.utils.Printer
+
+// This is the main entry point of the application.
+// It uses the `Printer` class from the `:utils` subproject.
+fun main() {
+    val message = "Hello JetBrains!"
+    val printer = Printer(message)
+    printer.printMessage()
+}
